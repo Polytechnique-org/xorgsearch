af2264d5452ff9c8a8b952458d9e903de6cac2c0
Victor Berger <victor.berger@m4x.org>
2016-10-15 17:31:55 +0000

    Add support of filters

--- src/xorgsearch/profiles.py
+++ src/xorgsearch/profiles.py
@@ -23,12 +23,40 @@ class ProfileSearch(FacetedSearch):
     }
 
     def query(self, search, query):
-        for term in query.split():
+        (raw_terms, matched) = split_terms(query, self.fields)
+        unmatched_fields = [ f for f in self.fields if f not in matched ]
+
+        for term in raw_terms:
             q = None
-            for field in self.fields:
+            for field in unmatched_fields:
                 if q is None:
                     q = Q({"fuzzy": { field: term }})
                 else:
                     q = q | Q({"fuzzy": { field: term}})
             search = search.query(q)
+        for (field, values) in matched.items():
+            q = None
+            for v in values:
+                if q is None:
+                    q = Q({"fuzzy": { field: v }})
+                else:
+                    q = q | Q({"fuzzy": { field: v }})
+            search = search.query(q)
         return search
+
+def split_terms(search_string, fields):
+    matched = {}
+    raw_terms = []
+    for term in search_string.split():
+        if ':' in term:
+            [name, value] = term.split(':', maxsplit=2)
+            if name in fields:
+                if name not in matched:
+                    matched[name] = [value]
+                else:
+                    matched[name].append(value)
+            else:
+                raw_terms.append(term)
+        else:
+            raw_terms.append(term)
+    return (raw_terms, matched)
